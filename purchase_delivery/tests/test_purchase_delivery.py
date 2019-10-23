3c6128908489959a7ee0926bf93fba29fa025cb9
ahenriquez <ahenriquez@eficent.com>
2019-10-23 15:23:20 +0200

    [MIG]purchase_delivery to v12

--- purchase_delivery/tests/test_purchase_delivery.py
+++ purchase_delivery/tests/test_purchase_delivery.py
@@ -1,6 +1,3 @@
-# Copyright 2018 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from datetime import datetime
 
 from odoo.tests import common
